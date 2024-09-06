1d5efa1b91c7a710cdfe2760466e5c119fc74ebb
gerardo <gerardo.sosa@farbenmeer.de>
2024-09-06 12:30:07 +0200

    chore: Please linter & formatter

--- botMessages.ts
+++ botMessages.ts
@@ -3,28 +3,28 @@ const MESSAGES: { [key: string]: string } = {};
 
 MESSAGES.START = `
 Hello! I live! I am your friendly weekly reminder bot 🤖\n
-Use <code>/reminders-on</code> (<code>/reminders-off</code>) to (un)subscribe to my weekly notifications! 📅`
+Use <code>/reminders-on</code> (<code>/reminders-off</code>) to (un)subscribe to my weekly notifications! 📅`;
 
 MESSAGES.STATUS = `
 I'm doing just fine, buddy. Thanks for asking 🫶
-`
+`;
 
 MESSAGES.SUBSCRIBED = `
 I've been unleashed!
 🔥 You shall endure the full might of my reminders! 🔥
-... that is, of course, once on Mondays and again once on Fridays ✅`
+... that is, of course, once on Mondays and again once on Fridays ✅`;
 
 MESSAGES.UNSUBSCRIBED = `
 I wait in an enforced slumber biding my time, 
 until the fated moment when I shall be set free once again, 
-to reignite with my reminders upon your unorganized world! 🧞`
+to reignite with my reminders upon your unorganized world! 🧞`;
 
 MESSAGES.SUBSCRIBED_FAIL = `Chat already susbscribed to /remiders`;
 
 MESSAGES.UNSUBSCRIBED_FAIL = `Your chat is not even on my list, bby 🧞`;
 
-MESSAGES.REMINDERS_CLEANING_PLAN_EVEN_WEEK = `🧹🪥 This is an automated Putzplan reminder! Left nothing behind! 🧼🫧`;
-MESSAGES.REMINDERS_CLEANING_PLAN_ODD_WEEK = `🧹🪥 This is an automated Putzplan reminder! Shine right like a diamond 🧼🫧`;
+MESSAGES.REMINDERS_CLEANING_PLAN_EVEN_WEEK = `🧹🪥 This is an automated Putzplan reminder! Left (side) nothing behind! 🧼🫧`;
+MESSAGES.REMINDERS_CLEANING_PLAN_ODD_WEEK = `🧹🪥 This is an automated Putzplan reminder! Shine right (side) like a diamond 🧼🫧`;
 
 MESSAGES.REMINDERS_VEGTABLE_BOX = `🌶️🥦🥕 This is an automated Gemüsekiste reminder! 🔥🌶️🔥\n
 Unleash your full potential by picking up that box today!`;
